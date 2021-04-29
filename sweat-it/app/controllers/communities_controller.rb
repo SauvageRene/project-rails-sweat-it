40d8879e8636cd929ddb843e509ec99f358eba8b
Rene <renegarcia1864@gmail.com>
2021-04-29 00:05:24 -0500

    Bio bugs when creating

--- sweat-it/app/controllers/communities_controller.rb
+++ sweat-it/app/controllers/communities_controller.rb
@@ -1,5 +1,6 @@
 class CommunitiesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_community, only: [:show]
     
     def index
         @communities = Community.all
@@ -15,7 +16,7 @@ class CommunitiesController < ApplicationController
 
     def create
         @community = Community.new(community_values)
-        @community.account_id = current_user.id
+        @community.user_id = current_user.id
 
         if @community.save
             redirect_to communities_path
@@ -25,6 +26,11 @@ class CommunitiesController < ApplicationController
     end
 
     private
+
+
+    def set_community
+        @community = Community.find(params[:id])
+    end
 
     def community_values
         params.require(:community).permit(:name, :url, :bio)
