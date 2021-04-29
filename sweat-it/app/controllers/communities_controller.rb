ce0f71b4d199b5171a627af63c9f75c697071c93
Rene <renegarcia1864@gmail.com>
2021-04-28 23:51:53 -0500

    buttons

--- sweat-it/app/controllers/communities_controller.rb
+++ sweat-it/app/controllers/communities_controller.rb
@@ -14,6 +14,20 @@ class CommunitiesController < ApplicationController
     end
 
     def create
+        @community = Community.new(community_values)
+        @community.account_id = current_user.id
+
+        if @community.save
+            redirect_to communities_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def community_values
+        params.require(:community).permit(:name, :url, :bio)
     end
 
 end
