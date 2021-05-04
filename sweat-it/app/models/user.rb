75262e92deb61fcbed4da10a8896602abc92997a
Rene <renegarcia1864@gmail.com>
2021-05-03 19:29:11 -0500

    Username and profile

--- sweat-it/app/models/user.rb
+++ sweat-it/app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
     has_many :communities
     has_many :posts
 
+    validates_presence_of :first_name, :last_name, :username
+
     def full_name
       "#{first_name} #{last_name}"
     end
--- sweat-it/config/routes.rb
+++ sweat-it/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "u/:username" => "profile#index", as: :profile
+  get "u/:username" => "public#profile", as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :communities do
     resources :posts
