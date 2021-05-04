ea7d0e6ae2094842d2aef7423ca37cb93a78b32e
Rene <renegarcia1864@gmail.com>
2021-05-03 19:00:50 -0500

    Popular posts updated, links to community id via post

--- sweat-it/config/routes.rb
+++ sweat-it/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "u/:username" => "profile#index", as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :communities do
     resources :posts
