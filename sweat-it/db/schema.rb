a5da671ec2be64b45f5511d3a596766f0652266a
Rene <renegarcia1864@gmail.com>
2021-05-05 16:21:56 -0500

    Trying to Set up Oauth

--- sweat-it/db/schema.rb
+++ sweat-it/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_191120) do
+ActiveRecord::Schema.define(version: 2021_05_05_203449) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2021_05_05_191120) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
