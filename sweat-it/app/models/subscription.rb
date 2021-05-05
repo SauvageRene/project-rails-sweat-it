7fc6345f87971aa7cf239c8da434d187d749f118
Rene <renegarcia1864@gmail.com>
2021-05-05 15:31:49 -0500

    New migration/Gemfile update

--- sweat-it/app/models/subscription.rb
+++ sweat-it/app/models/subscription.rb
@@ -1,4 +1,12 @@
 class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :community
+
+    # def admin
+    #     if Community.subscribers.first == self.user_id 
+            
+
+    #     end
+    # end
+    
 end
