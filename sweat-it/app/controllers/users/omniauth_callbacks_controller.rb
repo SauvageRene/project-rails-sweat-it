d77baadf37a3133e534a8a0c357586a8574c81f6
Rene <renegarcia1864@gmail.com>
2021-05-06 21:08:30 -0500

    Fixing OmniAuth

--- sweat-it/app/controllers/users/omniauth_callbacks_controller.rb
+++ sweat-it/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token, only: :google
+    skip_before_action :verify_authenticity_token, only: :google_oauth2, :facebook, :github 
 
   def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -17,30 +17,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-  sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-  set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
+    sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+    set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
     else
     session["devise.github"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
     redirect_to new_user_registration_url
     end
   end
 
-  def facebook
-  @user = User.from_omniauth(request.env["omniauth.auth"])
+  def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-if @user.persisted?
-sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-else
-session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
-redirect_to new_user_registration_url
-end
-end
+      if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      else
+      session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url
+      end
+  end
 
 
 
 
   def failure
+    flash[:error] = 'There was a porblem signing you in. Please register or try signing in later.'
     redirect_to root_path
   end
 
